7e7a92689c0ef07c2b1dcdd878d41a3412f7cffd
maverickjh <maverickjh@gmail.com>
2014-03-20 19:41:21 -0700

    sup

---
+++ TetrisAI/Piece.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisAI
+{
+    class Piece
+    {
+        public int Xpos { get; private set; }
+        public int Ypos { get; private set; }
+        private BitArray2D _colPiece;
+        public Piece(int x, int y, BitArray2D bits)
+        {
+            Xpos = x;
+            Ypos = y;
+            _colPiece = bits;
+        }
+        public void MoveRight()
+        {
+            ++Ypos;
+            ++Xpos;
+        }
+        public void MoveLeft()
+        {
+
+        }
+        public void SimulateGravity()
+        {
+
+        }
+    }
+}
